3913d7334533ccdd79246378297d72bed7098d14
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-10-04 20:16:03 +0100

    add Dockerfile for production

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-hamza baazaoui        </p>
+zzzzzzzzzzzzzzzzzzzzz       </p>
         <a
           className="App-link"
           href="https://reactjs.org"
